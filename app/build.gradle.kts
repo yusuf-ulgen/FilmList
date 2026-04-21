5f97264afaeb91c4b00004a87a8807ed89c53682
Yusuf Ulgen <ysfulgen142@gmail.com>
2026-04-21 23:18:13 +0300

    chore: versiyon yükseltildi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.yusufulgen.filmlist"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
