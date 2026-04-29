a90d613965c2840a67d67bc441f7476e2706b0ab
Yusuf Ulgen <ysfulgen142@gmail.com>
2026-04-29 21:49:14 +0300

    sürüm yükseltildi.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.yusufulgen.filmlist"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
